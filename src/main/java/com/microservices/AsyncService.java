a7e3db796f74ca1ce3b8584582e40ba2ea6115db
aarunbala <aarunbala@gmail.com>
2018-01-05 08:17:51 +1100

    Enabling Slueth & Zipkin

--- src/main/java/com/microservices/AsyncService.java
+++ src/main/java/com/microservices/AsyncService.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -26,11 +29,16 @@ import rx.schedulers.Schedulers;
 @Service
 public class AsyncService {
 
+    private Logger LOG = LoggerFactory.getLogger(AsyncService.class);
+
 	@Autowired
 	ProductRepository repo;
 	
 	@Autowired
 	AllergenRepository allergenRepo;
+	
+	@Autowired
+	private Tracer tracer;
 	
 
 	public DeferredResult<List<ProductVO>> getProducts(ProductRequest request) {
@@ -41,6 +49,7 @@ public class AsyncService {
 		profiles
 			.flatMap(profile -> getProductsForProfile(products, request.getProductIds(), profile))
 			.toSortedList(request.getProfiles().size())
+                .doOnError(er -> System.out.println("MAIN"+er))
 			.subscribe(m -> {
 				collateProductDetails(products, request.getProductIds());
 				deffered.setResult(products.getProducts());	
@@ -50,23 +59,33 @@ public class AsyncService {
 	}
 
 	public Observable<Products> getProductsForProfile(Products products, List<String> productIds, String profile) {
-		System.out.println("Inside getProductsForProfile : " + Thread.currentThread().getName());
+//		System.out.println("Inside getProductsForProfile : " + Thread.currentThread().getName());
+		LOG.info("Inside getProductsForProfile : {}", Thread.currentThread().getName());
 		return Observable
 				.just(profile)
 				.flatMap(pdt -> getProductFromDB(products, productIds, profile))
+                .onErrorReturn(er -> {
+                    er.printStackTrace();
+                    return new Products();
+                })
 				.subscribeOn(Schedulers.io());
 	}
 
 	public Observable<Products> getProductFromDB(Products products, List<String> productIds, String profile) {
 		System.out.println("Inside getProductFromDB : " + Thread.currentThread().getName());
-		
+		LOG.info("Inside getProductFromDB : {}" , Thread.currentThread().getName());
 		switch (profile) {
 			case "BASIC":
+				Span spanB = this.tracer.createSpan("Basic Database call");
 				products.setBasic(getBasicProductDetails(productIds));
+				this.tracer.close(spanB);
 				break;
 			case "ALLERGEN":
+				Span spanA = this.tracer.createSpan("Allergen Database call");
+//			    throw new RuntimeException("Database unavailable");
 				products.setAllergen(getAllergenDetails(productIds));
-				break;
+				this.tracer.close(spanA);
+                break;
 			case "LOCATION":
 				break;
 		}
@@ -127,10 +146,12 @@ public class AsyncService {
 	}
 	
 	public void addProducts(List<Product> products) {
+		LOG.info("Adding Products");
 		repo.save(products);
 	}
 
 	public void addAllergens(List<Allergen> allergens) {
+		LOG.info("Adding Allergens");
 		allergenRepo.save(allergens);
 	}
 }
